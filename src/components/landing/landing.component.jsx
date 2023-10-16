9c686b24e1d30ecd5203c81e18e2b656696c8b73
Jake Girhiny <jegirhiny@gmail.com>
2023-10-15 20:04:32 -0400

    Removed redundant css and cleaned up jsx

--- src/components/landing/landing.component.jsx
+++ src/components/landing/landing.component.jsx
@@ -3,12 +3,13 @@ import './landing.styles.css';
 const Landing = () => {
     return(
         <section className='landing-div'>
-            <div className='center-div'>
+            <div>
                 <h3 className='landing-my-name'>Hi, my name is</h3>
-                <h1 className='landing-name'>Jake Girhiny.</h1>
-                <h2 className='landing-job'>Software Engineer.</h2>
-                <p className='landing-about'>
-                    A Massachusetts student deeply passionate about software development. I'm immersed in all aspects of applications, including design, testing, and the use of code to address real-life challenges. Feel free to explore - you're likely to find something that sparks your interest.</p>
+                <h1>Jake Girhiny</h1>
+                <h2>Software Engineer</h2>
+                <p>
+                    A Massachusetts student deeply passionate about software development. I'm immersed in all aspects of applications, including design, testing, and the use of code to address real-life challenges. Feel free to explore - you're likely to find something that sparks your interest.
+                </p>
                 <a href='#about' className='landing-more'>Get to know me!</a>
             </div>
         </section>
